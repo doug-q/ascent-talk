47a91463689e99a9fdecc65f373546849944c484
Douglas Wilson <douglas.wilson@quantinuum.com>
2024-06-27 15:24:27 +0100

    init

---
+++ devenv.nix
@@ -0,0 +1,25 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  packages = [
+    pkgs.texlive.combined.scheme-full
+    pkgs.python3Packages.pygments
+  ];
+
+
+
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/languages/
+  # languages.nix.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
